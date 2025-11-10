f56a07289d5121ffd632becf8151050c2a2cae57
hann <wjdgks1233@gmail.com>
2025-11-10 21:11:54 +0900

    feat : swagger setting #1

--- src/main/java/com/kw/Ddareungi/global/config/JpaAuditingConfig.java
+++ src/main/java/com/kw/Ddareungi/global/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package com.kw.Ddareungi.common.config;
+package com.kw.Ddareungi.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
