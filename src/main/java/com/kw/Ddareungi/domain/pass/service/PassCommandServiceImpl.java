4d702675d64567645f4dc5ddd003525bdd1b5a15
hann <wjdgks1233@gmail.com>
2025-11-19 17:09:33 +0900

    chore : passcommandservice #4

--- src/main/java/com/kw/Ddareungi/domain/pass/service/PassCommandServiceImpl.java
+++ src/main/java/com/kw/Ddareungi/domain/pass/service/PassCommandServiceImpl.java
@@ -12,12 +12,20 @@ public class PassCommandServiceImpl implements PassCommandService {
     private final PassRepository passRepository;
 
 
+    /**
+     * SQL 입력
+     * 1. passId와 username 존재 유무 확인(Exist)
+     * 2. argument로 create User_Pass
+     * 3. condition 1: price counting <- 사용자가 가지고 있는 credit 차감 필요
+     * 3-1. 필요하다면 별도의 SQL 작성
+     *
+     * @param passId
+     * @param username
+     * @return
+     */
     @Override
     public Long buyPass(Long passId, String username) {
-        /**
-         * create user_pass sql
-         * condition 1: price counting
-         */
+
         return 0L;
     }
 }
