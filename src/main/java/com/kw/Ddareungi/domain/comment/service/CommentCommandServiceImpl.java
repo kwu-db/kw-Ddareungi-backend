b00eeca4aa4ebe5dc3dd5f0c24b0a796387d819e
hann <wjdgks1233@gmail.com>
2025-11-19 17:05:19 +0900

    chore : commentcommandservice #4

--- src/main/java/com/kw/Ddareungi/domain/comment/service/CommentCommandServiceImpl.java
+++ src/main/java/com/kw/Ddareungi/domain/comment/service/CommentCommandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kw.Ddareungi.domain.comment.service;
 
+import com.kw.Ddareungi.domain.comment.dto.request.RequestComment;
 import com.kw.Ddareungi.domain.comment.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,4 +12,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommentCommandServiceImpl implements CommentCommandService {
 
     private final CommentRepository commentRepository;
+
+    /**
+     * SQL 입력
+     * 1. boardId와 username을 통해 Board, User 존재 유무 확인(Exist)
+     * 2. argument에 맞게 데이터 입력
+     * @param boardId
+     * @param username
+     * @param requestComment
+     * @return
+     */
+    @Override
+    public Long writeCommentAtBoard(Long boardId, String username, RequestComment requestComment) {
+        return 0L;
+    }
 }
