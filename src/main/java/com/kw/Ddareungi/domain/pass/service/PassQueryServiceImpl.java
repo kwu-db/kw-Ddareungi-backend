e40e2e5dc2fc8ddbbf3c493c8966b30090636ebf
hann <wjdgks1233@gmail.com>
2025-11-19 17:10:46 +0900

    chore: passqueryservice #4

--- src/main/java/com/kw/Ddareungi/domain/pass/service/PassQueryServiceImpl.java
+++ src/main/java/com/kw/Ddareungi/domain/pass/service/PassQueryServiceImpl.java
@@ -13,12 +13,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class PassQueryServiceImpl implements PassQueryService {
     private final PassRepository passRepository;
 
+
     @Override
     public ResponsePassList getPassList() {
         //query all pass
         return null;
     }
 
+    /**
+     * SQL 입력
+     * 1. username을 통해 user_pass, pass 조회(where)
+     * 2. return에 맞게 매핑
+     * @param username
+     * @return ResponseUserPassList
+     */
     @Override
     public ResponseUserPassList getUserPassList(String username) {
         //query pass and join user on user.username=:username
